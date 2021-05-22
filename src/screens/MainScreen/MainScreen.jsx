866108c79a8f8830b88b72c401f54981d7e4db7f
Olexiy665 <alexey6mk@gmail.com>
2021-05-22 18:28:01 +0300

    add main screen

--- src/screens/MainScreen/MainScreen.jsx
+++ src/screens/MainScreen/MainScreen.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './MainScreen.module.scss';
 // import PropTypes from 'prop-types';
 
 export const MainScreen = () => (
-        <div>
+        <div className={styles.MainScreen}>
             Main App
         </div>
     );
