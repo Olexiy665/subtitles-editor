ee33c4067491c95b85c15782759103feaab6a0bd
Olexiy665 <alexey6mk@gmail.com>
2021-05-22 17:15:00 +0300

    [setup] css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.scss";
+import styles from  "./App.module.scss";
 
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
