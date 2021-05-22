d31f066e3b9488b4e98fb62ede23712e7d23c069
Olexiy665 <alexey6mk@gmail.com>
2021-05-22 18:53:45 +0300

    [setup] add redux

---
+++ src/features/SubtitlesList/SubtitlesList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SubtitlesList = () => (
+        <div>
+            List
+        </div>
+    );
+
+SubtitlesList.propTypes = {
+    
+};
+
+export default SubtitlesList;
