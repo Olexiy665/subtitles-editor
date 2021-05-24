1d6a4877dab237ef750c29d97a193cb0a02a01f8
Olexiy665 <alexey6mk@gmail.com>
2021-05-24 12:50:14 +0300

    feature/higlhights

--- src/features/SubtitlesPlayer/SubtitlesPlayer.jsx
+++ src/features/SubtitlesPlayer/SubtitlesPlayer.jsx
@@ -2,30 +2,47 @@ import React, { useEffect, useState } from "react";
 
 import IntervalTree from "@flatten-js/interval-tree";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { SUBTITLES } from "../../constants/subtitles";
 import styles from "./SubtitlesPlayer.module.scss";
+import { updateActiveSubtitle  } from "./subtitlesPlayerSlice";
 
 export const SubtitlesPlayer = () => {
   const storeCurrentTime = useSelector((state) => state.player.currentTime);
+  const currentSubtitle = useSelector((state) => state.subtitles.activeSubtitle);
+  const dispatch = useDispatch();
   const [normalizedTree, setNormalizedTree] = useState(new IntervalTree());
+
   useEffect(() => {
     const tree = new IntervalTree();
     SUBTITLES.forEach((item) => {
-      tree.insert([item.startTime, item.endTime], item.text);
+      tree.insert([item.startTime, item.endTime], item);
     });
     setNormalizedTree(tree);
   }, []);
-  const searchResults = normalizedTree.search([
-    storeCurrentTime,
-    storeCurrentTime,
-  ]);
+
+  useEffect(() => {
+    const nextCurrentSubtitle = normalizedTree.search([
+      storeCurrentTime,
+      storeCurrentTime,
+    ])[0];
+    if (
+      nextCurrentSubtitle &&
+      nextCurrentSubtitle.text !== currentSubtitle?.text
+    ) {
+
+      dispatch(updateActiveSubtitle( {activeSubtitle: nextCurrentSubtitle}))
+      
+    } else if (!nextCurrentSubtitle?.text && currentSubtitle?.text) {
+      dispatch(updateActiveSubtitle( {activeSubtitle: {}}))
+    }
+  }, [storeCurrentTime]);
 
   return (
     <div className={styles.SubtitlesWrapper}>
-      {searchResults.map((item) => (
-        <div className={styles.SubtitleItem}>{item}</div>
-      ))}
+      {currentSubtitle.text && (
+        <div className={styles.SubtitleItem}>{currentSubtitle.text}</div>
+      )}
     </div>
   );
 };
